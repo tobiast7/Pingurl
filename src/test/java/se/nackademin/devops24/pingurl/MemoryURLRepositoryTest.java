541768a5594d3885967de5d78cb929a6fd1e82ca
tobias <tobiasostman1996@outlook.com>
2025-11-25 19:34:02 +0100

    uppdatede missad kod och imports

--- src/test/java/se/nackademin/devops24/pingurl/MemoryURLRepositoryTest.java
+++ src/test/java/se/nackademin/devops24/pingurl/MemoryURLRepositoryTest.java
@@ -1,3 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.nackademin.devops24.pingurl.model.PingedURL;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 class MemoryURLRepositoryTest {
 
     @Test
@@ -6,6 +16,15 @@ class MemoryURLRepositoryTest {
         repo.save("Test", "https://example.com");
 
         assertEquals(1, repo.getUrls().size());
+    }
+
+    @Test
+    void testGetUrls() {
+        MemoryURLRepository repo = new MemoryURLRepository();
+        repo.save("One", "https://one.com");
+        repo.save("Two", "https://two.com");
+
+        assertEquals(2, repo.getUrls().size());
     }
 
     @Test
